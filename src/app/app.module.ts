d27ee098417ac1a9dfb68d04bdd381ccb377a73f
Antti Nieminen <anttihn@gmail.com>
2017-05-05 19:37:05 +0300

    message

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { GraafiComponent } from './graafi/graafi.component';
+import { MessageComponent } from './message/message.component';
 
 import { Network, DataSet, Node, Edge, IdType } from 'vis';
 
 @NgModule({
   declarations: [
     AppComponent,
-    GraafiComponent
+    GraafiComponent,
+    MessageComponent
   ],
   imports: [
     BrowserModule,
